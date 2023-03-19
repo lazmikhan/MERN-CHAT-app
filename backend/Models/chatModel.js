dadc6881fc1615f1e2315bb7a19871c205256be4
Toya Lazmi Khan <86733350+lazmikhan@users.noreply.github.com>
2023-03-20 01:15:27 +0800

    added mongoose

---
+++ backend/Models/chatModel.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const chatModel = mongoose.Schema(
+    {
+        chatName:{type:String,trim:true},
+        isGroupChat:{type:Boolean},
+        
+    
+    }
+)
