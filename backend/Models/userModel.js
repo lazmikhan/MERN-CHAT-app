8b40b6aacb0cead415013c5ccbfc95679df92c81
Toya Lazmi Khan <86733350+lazmikhan@users.noreply.github.com>
2023-03-20 13:10:30 +0800

    aded all mongoose schemes

---
+++ backend/Models/userModel.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const userSchema=mongoose.Schema(
+    {
+       name: {
+        type:String , trim:true,required:true
+       },
+       email: {
+        type:String , trim:true,required:true
+       },
+       password: {
+        type:String , trim:true,required:true
+       },
+       picture: {
+        type:String , trim:true,required:true,default:'https://thumbs.dreamstime.com/b/default-avatar-profile-icon-social-media-user-vector-image-icon-default-avatar-profile-icon-social-media-user-vector-image-209162840.jpg'
+       }
+    }, {timestamps:true}
+)
+
+const User = mongoose.model('User', userSchema);
+module.exports= User; 
